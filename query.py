81d290e92c319b5a6bd4794b77bc0622f17d6d26
Amy Li <amy_li1@brown.edu>
2020-12-21 22:20:36 -0500

    sites

--- query.py
+++ query.py
@@ -1,34 +1,54 @@
 import requests
 import csv
 
-# open file with urls
 
-# setup query fields
+# open output file and write headers
+output = csv.writer(open("data/web-traffic/batch2.csv", "a"))
+# output.writerow(["Url", "Date", "PageviewsPerMillion", "PageviewsPerUser", "Rank", "ReachPerMillion"])
+
+
+# setup universal query fields
 API_key = "7H9WrorbrZ2gNI3U696P64wQe6jKlG5Xa9CQakn7"
-url = "zoom.us"
-company = "zoom"
 headers = {'x-api-key': API_key}
-startdate = "20180501"
 
-# make API call
-query = "https://awis.api.alexa.com/api?Action=TrafficHistory&Range=31&ResponseGroup=History&Start=" + startdate + "&Url=" + url + "&Output=json"
-response = requests.get(query, headers=headers)
-data = response.json()
 
-# extract fields from response
-siteData = data["Awis"]["Results"]["Result"]["Alexa"]["TrafficHistory"]["HistoricalData"]["Data"]
-argumentData = data["Awis"]["Results"]["Result"]["Alexa"]["Request"]["Arguments"]["Argument"]
-urlOne = [x["Value"] for x in argumentData if x["Name"] == "url"]
-url = urlOne[0]
+# put start dates into dictionary
+start_dates = {}
+with open("data/start-dates.csv", "r") as csvFile:
+    reader = csv.DictReader(csvFile, fieldnames=("start", "length"))
+    for row in reader:
+        start_dates[row["start"]] = row["length"]
+    csvFile.close()
 
-# write response to file
-f = csv.writer(open("data/" + company + ".csv", "w"))
-f.writerow(["Url", "Date", "PageviewsPerMillion", "PageviewsPerUser", "Rank", "ReachPerMillion"])
 
-for row in siteData:
-    f.writerow([url,
-                row["Date"],
-                row["PageViews"]["PerMillion"],
-                row["PageViews"]["PerUser"],
-                row["Rank"],
-                row["Reach"]["PerMillion"]])
+def make_API_call(url, startdate, length):
+    # make API call
+    query = "https://awis.api.alexa.com/api?Action=TrafficHistory&Range=" + length + "&ResponseGroup=History&Start=" + startdate + "&Url=" + url + "&Output=json"
+    response = requests.get(query, headers=headers)
+    data = response.json()
+
+    # extract fields from response
+    siteData = data["Awis"]["Results"]["Result"]["Alexa"]["TrafficHistory"]["HistoricalData"]["Data"]
+    argumentData = data["Awis"]["Results"]["Result"]["Alexa"]["Request"]["Arguments"]["Argument"]
+    urlOne = [x["Value"] for x in argumentData if x["Name"] == "url"]
+    url = urlOne[0]
+
+    # write response to file
+    for row in siteData:
+        output.writerow([url,
+                    row["Date"],
+                    row["PageViews"]["PerMillion"],
+                    row["PageViews"]["PerUser"],
+                    row["Rank"],
+                    row["Reach"]["PerMillion"]])
+
+
+# loop over urls and make queries for each
+with open("data/consolidated-sites2.txt", "r") as f:
+    urls = f.read().splitlines()
+    for url in urls:
+        print(url)
+        # make api call for every start date in year (2018)
+        for startDate in start_dates:
+            length = start_dates[startDate]
+            make_API_call(url, startDate, length) 
